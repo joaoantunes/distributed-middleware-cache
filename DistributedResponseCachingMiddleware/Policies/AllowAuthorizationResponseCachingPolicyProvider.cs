2e460373c3bf7160fb72bf850851e80975357dbb
Joao Antunes <Joao.Antunes@faro.com>
2020-07-14 15:34:32 +0100

    Added AllowAuthorizationResponseCachingPolicyProvider that allows cache when Authorization Header exists. Added a simple sample using DistributedCache.

---
+++ DistributedResponseCachingMiddleware/Policies/AllowAuthorizationResponseCachingPolicyProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.ResponseCaching.Internal;
+
+/// <summary>
+/// Overrides the default behaviour of ResponseCachingPolicyProvider
+/// and allows requests with Authorization header to be cached
+/// </summary>
+public class AllowAuthorizationResponseCachingPolicyProvider : ResponseCachingPolicyProvider
+{
+    public override bool AttemptResponseCaching(ResponseCachingContext context)
+    {
+        var request = context.HttpContext.Request;
+        if (!HttpMethods.IsGet(request.Method) && !HttpMethods.IsHead(request.Method))
+        {
+            context.Logger.LogRequestMethodNotCacheable(request.Method);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ DistributedResponseCachingMiddleware/Policies/CustomResponseCachingPolicyProviderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.ResponseCaching.Internal;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	/// <summary>
+	/// Extension methods for the ResponseCaching Policies.
+	/// </summary>
+	public static class CustomResponseCachingPolicyProviderExtensions
+	{
+		/// <summary>
+		/// Add policy that allows caching even if it exists an Authorization header.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/> for adding services.</param>
+		/// <returns></returns>
+		public static IServiceCollection AddAllowAuthorizationResponseCachingPolicy(this IServiceCollection services)
+		{
+			if (services == null)
+			{
+				throw new ArgumentNullException(nameof(services));
+			}
+
+            services.Add(ServiceDescriptor.Singleton<IResponseCachingPolicyProvider, AllowAuthorizationResponseCachingPolicyProvider>());
+			return services;
+		}
+    }
+}
